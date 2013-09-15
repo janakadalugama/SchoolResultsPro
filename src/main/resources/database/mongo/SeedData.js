b686aed64cd13d855bb589b14c6306eec735cbf7
unknown <janakadalugama@gmail.com>
2013-09-15 14:45:32 +0530

    added seed data for school

--- src/main/resources/database/mongo/SeedData.js
+++ src/main/resources/database/mongo/SeedData.js
@@ -19,3 +19,153 @@ db.subjects.insert({
 	description  : "Spelling",
 	code : "Spelling"
 })
+
+db.schools.insert({
+
+	name : "St. Victor College",
+	code : "stvc"
+})
+
+db.schools.insert({
+
+	name : "Adelong Public school",
+	code : "Adps"
+})
+
+db.schools.insert({
+
+	name : "Albert Park Public School",
+	code : "apps"
+})
+
+db.schools.insert({
+
+	name : "Albury High School",
+	code : "alhs"
+})
+
+db.schools.insert({
+
+	name : "Balmian Public School",
+	code : "baps"
+})
+
+db.schools.insert({
+
+	name : "Bankstown Girls High School",
+	code : "bghs"
+})
+
+db.schools.insert({
+
+	name : "Cambridge Park High School",
+	code : "cphs"
+})
+
+db.schools.insert({
+
+	name : "Camden High School",
+	code : "cahs"
+})
+
+db.schools.insert({
+
+	name : "Dawson Public School",
+	code : "daps"
+})
+
+db.schools.insert({
+
+	name : "Dudley Public School",
+	code : "dups"
+})
+
+db.schools.insert({
+
+	name : "Eastwood Public School",
+	code : "ewps"
+})
+
+db.schools.insert({
+
+	name : "Evans High School",
+	code : "evhs"
+})
+
+db.schools.insert({
+
+	name : "Fairfield High School",
+	code : "ffhs"
+})
+
+db.schools.insert({
+
+	name : "Franklin Public School",
+	code : "frps"
+})
+
+db.schools.insert({
+
+	name : "Georges Hall Public School",
+	code : "ghps"
+})
+
+db.schools.insert({
+
+	name : "Gordon West Public School",
+	code : "gwps"
+})
+
+db.schools.insert({
+
+	name : "Helensburg Public School",
+	code : "hbps"
+})
+
+db.schools.insert({
+
+	name : "Hunters Hill High School",
+	code : "hhhs"
+})
+
+db.schools.insert({
+
+	name : "Johns River Public School",
+	code : "jrps"
+})
+
+db.schools.insert({
+
+	name : "Kendall Public School",
+	code : "keps"
+})
+
+db.schools.insert({
+
+	name : "Lawrence Public School",
+	code : "laps"
+})
+
+db.schools.insert({
+
+	name : "Liverpool Girls High School",
+	code : "lghs"
+})
+
+db.schools.insert({
+
+	name : "Maitland High School",
+	code : "mlhs"
+})
+
+db.schools.insert({
+
+	name : "Milton Public School",
+	code : "mips"
+})
+
+db.schools.insert({
+
+	name : "Newcastle High School",
+	code : "nchs"
+})
